99e378c5d45eafb1eef9bf1d1a485e2d125392a0
Chris Allen <chris.allen@puppet.com>
2023-02-27 13:06:31 +0000

    Initial Release

---
+++ lib/puppet/functions/boltfunctions/configprint.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'open3'
+require 'bolt/error'
+
+# Return the output of print config
+Puppet::Functions.create_function(:'boltfunctions::configprint') do
+  # @param config The config that requires printing.
+  # @return The config value.
+  # @example Print the server name
+  #   boltfunctions::configprint('server')
+  dispatch :configprint do
+    required_param 'String', :config
+    optional_param 'String', :environment
+    optional_param 'String', :section
+    return_type 'String'
+  end
+
+  def command(command, arguments = [])
+    stdout, stderr, p = Open3.capture3(command, *arguments)
+    { stdout: stdout,
+        stderr: stderr,
+        exit_code: p.exitstatus }
+  end
+
+  def configprint(config, environment = nil, section = nil)
+    # Send Analytics Report
+    Puppet.lookup(:bolt_executor) {}&.report_function_call(self.class.name)
+
+    if !environment.nil?
+      environment_string = '--environment #{environment}'
+    else
+      environment_string = ''
+    end
+
+    if !section.nil?
+      section_string = '--section #{section}'
+    else
+      section_string = ''
+    end  
+
+    command_string = "puppet config print #{config} #{environment_string} #{section_string}"
+
+    result = command(command_string)
+
+    if result[:exit_code] != 0
+      raise Bolt:Error.new("Could not print config: #{result[:stderr]}")
+    end
+
+    result[:stdout].chomp
+  end
+
+end
---
+++ plans/printconfig.pp
@@ -0,0 +1,16 @@
+# This is the structure of a simple plan. To learn more about writing
+# Puppet plans, see the documentation: http://pup.pt/bolt-puppet-plans
+
+# The summary sets the description of the plan that will appear
+# in 'bolt plan show' output. Bolt uses puppet-strings to parse the
+# summary and parameters from the plan.
+# @summary Test the printconfig function
+# @param targets The targets to run on.
+plan boltfunctions::printconfig (
+  TargetSpec $targets = "localhost",
+  String $config = 'server',
+) {
+  $command_result = boltfunctions::configprint($server)
+
+  return $command_result
+}
